f427878d44f1d409a044f7c59e21400bcd5748d5
Kryxzael <kasper.ramskjell98@gmail.com>
2022-11-19 00:08:29 +0100

    Basic implementation

---
+++ WorkClock/Constants.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkClock
+{
+    /// <summary>
+    /// Contains common constants used throughout the program
+    /// </summary>
+    public static class Constants
+    {
+        public const int WeekLength = 5;
+
+        public static readonly TimeSpan DayStart = new(08, 0, 0);
+        public static readonly TimeSpan DayEnd   = new(16, 0, 0);
+
+        public static DateTime Now
+        {
+            get
+            {
+                return DateTime.Now;
+
+                DayOfWeek day = DayOfWeek.Wednesday;
+                TimeSpan time = new TimeSpan(11, 20, 00);
+
+                DateTime date = DateTime.Today;
+
+                while (date.DayOfWeek != DayOfWeek.Monday)
+                    date = date.AddDays(-1);
+
+                while (date.DayOfWeek != day)
+                    date = date.AddDays(1);
+
+                return date + time;
+            }
+        }
+
+        public static DurationProgressInfo ThisWeek
+        {
+            get
+            {
+                DateTime startDate = Now.Date;
+
+                while (startDate.DayOfWeek != DayOfWeek.Monday)
+                    startDate = startDate.AddDays(-1);
+
+                return new DurationProgressInfo(startDate + DayStart, startDate.AddDays(WeekLength - 1) + DayEnd);
+            }
+        }
+
+        public static DurationProgressInfo GetDaySpan(DateTime date)
+        {
+            return new DurationProgressInfo(date.Date + DayStart, date.Date + DayEnd);
+        }
+
+        public static TimeSpan GetDurationCompletedThisWeek()
+        {
+            TimeSpan time = default;
+
+            //DayOfWeek starts with Sunday at 0, so we need to make an exception for it in this case
+            if (Now.DayOfWeek == DayOfWeek.Sunday && System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek == DayOfWeek.Monday)
+                return GetTotalDurationThisWeek();
+
+            for (DayOfWeek day = DayOfWeek.Monday; day <= DayOfWeek.Friday; day++)
+            {
+                if (Now.DayOfWeek != day)
+                {
+                    time += DayEnd - DayStart;
+                }
+                else
+                {
+                    time += GetDaySpan(Now).GetTimeSinceStart();
+                    break;
+                }
+            }
+
+            return time;
+        }
+
+        public static TimeSpan GetTotalDurationThisWeek()
+        {
+            return (DayEnd - DayStart) * WeekLength;
+        }
+
+        public static string CreateBar(int inclusiveWidth, float fillPercentage, char start = '[', char fill = '#', char empty = ' ', char end = ']')
+        {
+            if (inclusiveWidth < 2)
+                throw new ArgumentOutOfRangeException(nameof(inclusiveWidth), "Inclusive width must be at least 2 to account for the start and end caps");
+
+            fillPercentage = Math.Clamp(fillPercentage, 0f, 1f);
+
+            StringBuilder str = new StringBuilder(new string(empty, inclusiveWidth));
+            str[0] = start;
+            str[^1] = end;
+
+            int exclusiveWidth = inclusiveWidth - 2;
+
+            if (exclusiveWidth > 0)
+            {
+                for (int i = 0; i < Math.Round(fillPercentage * exclusiveWidth); i++)
+                    str[i + 1] = fill;
+            }
+
+            return str.ToString();
+        }
+
+        public static string CreateTableEntry(string text, string val)
+        {
+            return text.PadRight(20) + val.PadLeft(20);
+        }
+
+        public static string CreateTableEntry(string text, DateTime time)
+        {
+            return CreateTableEntry(text, time.ToString("HH:mm:ss"));
+        }
+
+        public static string CreateTableEntry(string text, TimeSpan span)
+        {
+            if (span.Ticks < 0)
+                span = new TimeSpan(-span.Ticks);
+
+            else if (span == default)
+                return CreateTableEntry(text, "Zero");
+
+            return CreateTableEntry(text, (int)span.TotalHours + span.ToString("':'mm':'ss"));
+        }
+
+        public record DurationProgressInfo(DateTime Start, DateTime End)
+        {
+            public DurationProgressInfo(DateTime start, TimeSpan duration) : this(start, start + duration)
+            {  }
+
+            public TimeSpan GetTimeSinceStart()
+            {
+                return Now - Start;
+            }
+
+            public TimeSpan GetTimeUntilEnd()
+            {
+                return End - Now;
+            }
+
+            public float GetCompletionPercentage()
+            {
+                return (Now.Ticks - Start.Ticks) / (float)(End - Start).Ticks;
+            }
+
+            public string CreateBar(int inclusiveWidth, char start = '[', char fill = '#', char empty = ' ', char end = ']')
+            {
+                return Constants.CreateBar(inclusiveWidth, GetCompletionPercentage(), start, fill, empty, end);
+            }
+        }
+    }
+}
--- WorkClock/Program.cs
+++ WorkClock/Program.cs
@@ -1,12 +1,167 @@
 ﻿using System;
+using System.Threading;
 
 namespace WorkClock
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                Console.Clear();
+
+                int dayWidth = Console.BufferWidth / Constants.WeekLength;
+
+                for (TimeSpan time = Constants.DayStart; time < Constants.DayEnd; time += new TimeSpan(1, 0, 0))
+                {
+                    for (DateTime day = Constants.ThisWeek.Start.Date; day <= Constants.ThisWeek.End.Date; day = day.AddDays(1))
+                    {
+                        if (time.Hours == 11)
+                        {
+                            var progress      = new Constants.DurationProgressInfo(day + time,  new TimeSpan(0, 30, 0));
+                            var lunchProgress = new Constants.DurationProgressInfo(day + time + new TimeSpan(0, 30, 0), new TimeSpan(0, 30, 0));
+                            float sectionWidth  = dayWidth / 2f;
+
+
+                            Console.Write(progress.CreateBar(     (int)Math.Floor(sectionWidth),       end:   '|'));
+                            Console.Write(lunchProgress.CreateBar((int)Math.Ceiling(sectionWidth) - 1, start: '|', fill: '@'));
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            var progress = new Constants.DurationProgressInfo(day + time, new TimeSpan(1, 0, 0));
+
+                            Console.Write(progress.CreateBar(dayWidth - 1));
+                            Console.Write(" ");
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(Constants.CreateTableEntry("Today", Constants.Now.ToString("dddd MMM-d")));
+                Console.WriteLine(Constants.CreateTableEntry("Current Time", Constants.Now.ToString("HH:mm:ss")));
+
+                if (Constants.Now.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday)
+                {
+                    Console.WriteLine(Constants.CreateTableEntry("Next Workweek In", Constants.ThisWeek.Start.AddDays(7) - Constants.Now));
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Have a nice weekend!!");
+                    Thread.Sleep(500);
+                    continue;
+                }
+
+                string lunchText;
+
+                if (Constants.Now.TimeOfDay < new TimeSpan(11, 20, 00))
+                {
+                    lunchText = "Before";
+                }
+                else if (Constants.Now.TimeOfDay < new TimeSpan(11, 30, 00))
+                {
+                    lunchText = "Soon";
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                }
+                else if (Constants.Now.TimeOfDay > new TimeSpan(12, 00, 00))
+                {
+                    lunchText = "After";
+                }
+                else
+                {
+                    if (DateTime.Now.Millisecond < 500)
+                        lunchText = "In Progress";
+                    else
+                        lunchText = "";
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+
+                Console.WriteLine(Constants.CreateTableEntry("Lunch", lunchText));
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                string coreWorkhoursText = "Unknown";
+
+                if (Constants.Now.TimeOfDay > new TimeSpan(9, 0, 0) && Constants.Now.TimeOfDay < new TimeSpan(14, 0, 0))
+                    coreWorkhoursText = "Yes";
+
+                else
+                    coreWorkhoursText = "No";
+
+                Console.WriteLine(Constants.CreateTableEntry("In Core Workhours", coreWorkhoursText));
+
+                Console.WriteLine();
+
+                var today = Constants.GetDaySpan(Constants.Now);
+                TimeSpan sinceStart = today.GetTimeSinceStart();
+                TimeSpan untilEnd = today.GetTimeUntilEnd();
+
+                if (sinceStart < default(TimeSpan))
+                {
+                    Console.WriteLine(Constants.CreateTableEntry("Time Until Work", sinceStart));
+                }
+                else
+                {
+                    Console.Write(Constants.CreateTableEntry("Time At Work", sinceStart));
+                    Console.Write("  ");
+                    Console.WriteLine((today.GetCompletionPercentage() * 100).ToString("0").PadLeft(3) + "% Complete");
+                }
+
+                if (untilEnd > default(TimeSpan))
+                {
+                    if (untilEnd < new TimeSpan(0, 15, 0))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else if (untilEnd < new TimeSpan(1, 0, 0))
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    }
+
+                    if (untilEnd < new TimeSpan(0, 5, 0) && DateTime.Now.Millisecond < 500)
+                        Console.Write(Constants.CreateTableEntry("Time Left", ""));
+                    else
+                        Console.Write(Constants.CreateTableEntry("Time Left", untilEnd));
+
+                    Console.Write("  ");
+                    Console.WriteLine(((1f - today.GetCompletionPercentage()) * 100).ToString("0").PadLeft(3) + "% Left");
+
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Constants.CreateTableEntry("Overtime Worked", untilEnd));
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+
+                Console.WriteLine();
+
+                TimeSpan timeThisWeek     = Constants.GetDurationCompletedThisWeek();
+                TimeSpan timeLeftThisWeek = Constants.GetTotalDurationThisWeek() - timeThisWeek;
+                float    weekPercentage   = (float)timeThisWeek.Ticks / Constants.GetTotalDurationThisWeek().Ticks;
+
+                if (timeThisWeek > Constants.GetTotalDurationThisWeek())
+                    timeThisWeek = Constants.GetTotalDurationThisWeek();
+
+                if (timeLeftThisWeek.Ticks < 0)
+                    timeLeftThisWeek = default;
+
+                weekPercentage = Math.Clamp(weekPercentage, 0f, 1f);
+
+                Console.Write(Constants.CreateTableEntry("Time This Week", timeThisWeek));
+                Console.Write("  ");
+                Console.WriteLine((weekPercentage * 100).ToString("0").PadLeft(3) + "% Complete");
+
+                Console.Write(Constants.CreateTableEntry("Time Left This Week", timeLeftThisWeek));
+                Console.Write("  ");
+                Console.WriteLine(((1f - weekPercentage) * 100).ToString("0").PadLeft(3) + "% Left");
+
+                Thread.Sleep(500);
+
+            } while (true);
         }
     }
 }
